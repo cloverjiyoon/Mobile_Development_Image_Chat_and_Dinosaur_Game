9e18117697e106197841a5c56bb70b3c0902b27a
CloverJiyoon <wldbs8278@gmail.com>
2022-11-06 01:35:14 -0800

    Changed App icon

---
+++ Group33WebAPI/app/src/main/java/edu/northeastern/group33webapi/Firebase.java
@@ -0,0 +1,16 @@
+package edu.northeastern.group33webapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.analytics.FirebaseAnalytics;
+
+import android.os.Bundle;
+
+public class Firebase extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firebase);
+    }
+}
