448d21d2d0eb7fa639d7210b8c7dc5fc6f57706c
CloverJiyoon <wldbs8278@gmail.com>
2022-11-08 20:31:50 -0800

    Updated NoteAdapter.java

--- Group33WebAPI/app/src/main/java/edu/northeastern/group33webapi/Firebase/NoteAdapter.java
+++ Group33WebAPI/app/src/main/java/edu/northeastern/group33webapi/Firebase/NoteAdapter.java
@@ -7,25 +7,36 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import edu.northeastern.group33webapi.R;
+
 public class NoteAdapter extends RecyclerView.Adapter<NoteViewHolder> {
 
+    private final List<Note> notes;
 
     public NoteAdapter(List<Note> notes) {
+        this.notes = notes;
     }
 
     @NonNull
     @Override
     public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        return new NoteViewHolder(LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_received_history, parent, false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull NoteViewHolder holder, int position) {
-
+        holder.bindThisData(notes.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return notes != null ? notes.size() : 0;
     }
 }
